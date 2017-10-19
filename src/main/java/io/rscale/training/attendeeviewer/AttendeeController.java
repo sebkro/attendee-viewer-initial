fc32c9271695ebb90cde807e09b1954fcdd4ed9d
Sebastian Krome <sebastian.krome@cgi.com>
2017-10-19 14:00:23 +0200

    get companies from company service

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeController.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeController.java
@@ -1,5 +1,8 @@
 package io.rscale.training.attendeeviewer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,29 +14,38 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AttendeeController {
 
-	private RosterClient attendeeClient;
 	private CompanyClient companyClient;
 	
 	@Autowired
-	public AttendeeController(RosterClient attendeeClient, CompanyClient companyClient) {
-		this.attendeeClient = attendeeClient;
+	public AttendeeController(CompanyClient companyClient) {
 		this.companyClient = companyClient;
 	}
 	
     @RequestMapping("/")
     public String attendees(@RequestParam(value="page", required=false, defaultValue= "0") int page, Model model) {
-        model.addAttribute("attendees", attendeeClient.getAttendees(page).getContent());
+    	List<Attendee> attendees = new ArrayList<>();
+    	attendees.add(createAttendee());
+        model.addAttribute("attendees", attendees);
         return "list";
     }
 
     @RequestMapping("/{attendeeId}")
     public String attendee(@PathVariable(value="attendeeId") String attendeeId, Model model) {
-        Attendee attendee = attendeeClient.getAttendee(attendeeId);
+        Attendee attendee = createAttendee();
     	model.addAttribute("attendee", attendee);
     	if ( ! StringUtils.isEmpty(attendee.getCompanyGuid()) ) {
     		model.addAttribute("company", companyClient.getCompany(attendee.getCompanyGuid()));
     	}
         return "detail";
     }
+
+	private Attendee createAttendee() {
+		Attendee attendee = new Attendee();
+        attendee.setCompanyName("0f69d9dd-3ed8-4954-90d9-eae379a30f97	");
+        attendee.setFirstName("Sebastian");
+        attendee.setLastName("Krome");
+        attendee.setUuid("sebastiank");
+		return attendee;
+	}
     
 }
